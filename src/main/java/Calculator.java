fe1642a01c480442bee5ea1241219fd46845c13b
kimdonghyeon <sh2115521@naver.com>
2022-07-05 11:32:23 +0900

    사칙연산

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public static int add(String s) {
+        String[] sBits = s.split(" \\+ ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 + no2;
+    }
+
+    public static int subtract(String s) {
+
+
+        return 0;
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void 더하기() {
+        int rs = Calculator.add("10 + 20");
+
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 더하기_2() {
+        int rs = Calculator.add("20 + 20");
+
+        assertEquals(40, rs);
+    }
+
+    @Test
+    public void 더하기_3() {
+        int rs = Calculator.add("10 + 10");
+
+        assertEquals(20, rs);
+    }
+
+    @Test
+    public void 빼기(){
+        int rs = Calculator.subtract("10 - 10");
+
+        assertEquals(0, rs);
+    }
+
+    @Test
+    public void 빼기_2(){
+        int rs = Calculator.subtract("30 - 20");
+
+        assertEquals(10, rs);
+    }
+
+}

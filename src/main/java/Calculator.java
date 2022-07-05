98a6468de5bebbcfb9a0da6e1eb1e6af9ad17d98
kimdonghyeon <sh2115521@naver.com>
2022-07-05 11:38:08 +0900

    사칙개별 사칙연산

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,7 +9,27 @@ public class Calculator {
 
     public static int subtract(String s) {
 
+        String[] sBits = s.split(" - ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
 
-        return 0;
+        return no1 - no2;
+    }
+
+    public static int multiply(String s) {
+
+        String[] sBits = s.split(" \\* ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 * no2;
+    }
+
+    public static int divide(String s) {
+        String[] sBits = s.split(" / ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 / no2;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,36 +7,43 @@ public class AppTest {
     @Test
     public void 더하기() {
         int rs = Calculator.add("10 + 20");
-
         assertEquals(30, rs);
     }
 
     @Test
     public void 더하기_2() {
         int rs = Calculator.add("20 + 20");
-
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_3() {
         int rs = Calculator.add("10 + 10");
-
         assertEquals(20, rs);
     }
 
     @Test
     public void 빼기(){
         int rs = Calculator.subtract("10 - 10");
-
         assertEquals(0, rs);
     }
 
     @Test
     public void 빼기_2(){
         int rs = Calculator.subtract("30 - 20");
-
         assertEquals(10, rs);
+    }
+
+    @Test
+    public void 곱하기(){
+        int rs = Calculator.multiply("10 * 5");
+        assertEquals(50, rs);
+    }
+
+    @Test
+    public void 나누기(){
+        int rs = Calculator.divide("10 / 5");
+        assertEquals(2, rs);
     }
 
 }
